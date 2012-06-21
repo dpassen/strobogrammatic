6219b0e5fdd245a6001b35c9ade2bbb2ede02473
Derek Passen <dpassen1@gmail.com>
2012-06-20 22:05:38 -0400

    Add support for sixes and nines to appear.

--- lib/strobogrammatic.rb
+++ lib/strobogrammatic.rb
@@ -1,6 +1,12 @@
 module Strobogrammatic
     def strobogrammatic?
-        (to_s =~ /[23457]/).nil?
+        unless (to_s =~ /[23457]/).nil? && to_s.count("6") == to_s.count("9")
+            false
+        else
+            sixes = (0 .. to_s.length - 1).find_all { |i| to_s[i,1] == '6' }
+            nines = (0 .. to_s.length - 1).find_all { |i| to_s[i,1] == '9' }
+            sixes.zip(nines.reverse).collect { |i,j| i + j }.all? { |n| n == (to_s.length - 1) }
+        end
     end
 end
 
