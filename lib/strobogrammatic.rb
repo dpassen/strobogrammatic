8f36140b0e949562db3b72a5d6dc0355f8c358fa
Derek Passen <dpassen@texeltek.com>
2012-06-20 23:23:27 -0400

    Negative numbers are not strobogrammatic.
    Narrow the scope of the gem from all Numerics to Fixnums.

--- lib/strobogrammatic.rb
+++ lib/strobogrammatic.rb
@@ -1,6 +1,6 @@
 module Strobogrammatic
     def strobogrammatic?
-        unless (to_s =~ /[23457]/).nil? && to_s.count("6") == to_s.count("9")
+        unless (to_s =~ /[-23457]/).nil? && to_s.count("6") == to_s.count("9")
             false
         else
             sixes = (0 .. to_s.length - 1).find_all { |i| to_s[i,1] == '6' }
@@ -10,6 +10,6 @@ module Strobogrammatic
     end
 end
 
-class Numeric
+class Fixnum 
     include Strobogrammatic
 end
