f87bdaecce279e50de39daafa73da5d661f72a53
Derek Passen <dpassen@texeltek.com>
2012-06-21 20:20:47 -0400

    Remove unnecessary block.

--- lib/strobogrammatic.rb
+++ lib/strobogrammatic.rb
@@ -17,6 +17,6 @@ class Fixnum
         midpoint = (to_s.length / 2.0).ceil
         to_s[0...midpoint].split(//).each_with_index.map do |char, index|
             Strobogrammatic::strobogrammatic_pair?(char, to_s.reverse[index])
-        end.all? { |b| b == true } 
+        end.all?
     end
 end
